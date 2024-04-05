ab41cfe51ca46efc26679dd5d46b03fe932fde7e
Sayem Ahamed <95583680+Sayemahamed@users.noreply.github.com>
2024-04-06 02:46:36 +0600

    kind of done

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,41 @@
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 }
-void loop() {
-  for(int i =0;i<2;i++){
-    if(Serial.available()>0){
-      Serial.println(Serial.read());
+void reSet()
+{
+  digitalWrite(8, 0);
+  digitalWrite(9, 0);
+  digitalWrite(10, 0);
+  digitalWrite(11, 0);
+}
+void loop()
+{
+  reSet();
+  if (Serial.available() > 0)
+  {
+    int command = Serial.read();
+    if (command == 97)
+    {
+      digitalWrite(8, 1);
+      digitalWrite(11, 1);
+    }
+    else if (command == 98)
+    {
+      digitalWrite(9, 1);
+      digitalWrite(10, 1);
+    }
+    else if (command == 99)
+    {
+      digitalWrite(9, 1);
+      digitalWrite(11, 1);
+    }
+    else if (command == 100)
+    {
+      digitalWrite(10, 1);
+      digitalWrite(8, 1);
     }
   }
   delay(100);
